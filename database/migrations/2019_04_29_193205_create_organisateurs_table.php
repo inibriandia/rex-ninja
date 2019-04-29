876ddb3ed0b14cdb538b047eb84541bb30a07b55
inibriandia <inibriandia@gmail.com>
2019-04-29 17:04:39 -0300

    Ajouter des donnees dans la table organisateurs et mettre a jour le controlleur ressource des organisateurs

--- database/migrations/2019_04_29_193205_create_organisateurs_table.php
+++ database/migrations/2019_04_29_193205_create_organisateurs_table.php
@@ -15,6 +15,12 @@ class CreateOrganisateursTable extends Migration
     {
         Schema::create('organisateurs', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('nom', 45);
+            $table->string('prenom', 45);
+            $table->string('email', 45);
+            $table->string('telephone', 15);
+            $table->string('entreprise', 45);
+            $table->string('password', 45);
             $table->timestamps();
         });
     }
