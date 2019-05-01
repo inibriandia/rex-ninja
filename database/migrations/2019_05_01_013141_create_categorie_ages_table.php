7d850504052c13049277e9d7fe14d3b744dbf74a
inibriandia <inibriandia@gmail.com>
2019-04-30 22:34:02 -0300

    Ajouter des tables a la base de donnees

---
+++ database/migrations/2019_05_01_013141_create_categorie_ages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategorieAgesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorie_ages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorie_ages');
+    }
+}
