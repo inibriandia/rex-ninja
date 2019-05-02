190a4b33b38406b344147c339ad040fef35c8219
zen <jessica.doucet@outlook.com>
2019-05-02 15:21:32 -0300

    Ajouter EP2 dans api.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/EP2', 'API\EP2Controller@getEP2');
