6ed864cdc202903fb2db2f56d79283f230c4bd94
inibriandia <inibriandia@gmail.com>
2019-05-03 22:42:48 -0300

    Ajouter les methodes dans le modele de la table evenement pour assurer l'integrite des realations un a plusieurs

--- app/Evenement.php
+++ app/Evenement.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Evenement extends Model
 {
-    //
+    public function categorie()
+    {
+        return $this->belongsTo('App\Categorie');
+    }
+
+    public function categorieAge()
+    {
+        return $this->belongsTo('App\CategorieAge');
+    }
+
+    public function ambiance()
+    {
+        return $this->belongsTo('App\Ambiance');
+    }
+
+    public function organisateur()
+    {
+        return $this->belongsTo('App\Organisateur');
+    }
+
+    public function ville()
+    {
+        return $this->belongsTo('App\Ville');
+    }
 }
