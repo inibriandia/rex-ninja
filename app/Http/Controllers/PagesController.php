53f0756722864003a07b7842c4985828759b1787
inibriandia <inibriandia@gmail.com>
2019-05-09 13:06:52 -0300

    Concatener le json des questions a poser au client

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -26,29 +26,8 @@ class PagesController extends Controller
         $categories = Categorie::all();
         $categoriesAge = CategorieAge::all();
         $ambiance = Ambiance::all();
-        $typePayant = Evenement::all();
 
-        // return view('pages.choix')->with('categories', $categories);
-        //return view('pages.choix', compact('categories', 'categoriesAge', 'ambiance','typePayant'));
-        //$choix= Evenement::select('categories', 'categoriesAge', 'ambiance','typePayant')->get();
-
-        //json pour la table Ambiance
-        $choix = Ambiance::select('id', 'ambiance') ->get();
-        //json pour la Table categorie
-        $choix2 = Categorie::select('id', 'categorie') ->get();
-
-        //methode pour concatener le json mais le $data doit etre un objet json
-        /*$result = array();
-        foreach($data as $us){
-            array_push($result, $choix,$choix2);
-        }
-        $result = json_encode($result, JSON_FORCE_OBJECT);
-
-
-        return json_encode($result);
-        */
-        return json_encode($choix);
-
-
+        $questions = array('categoriesAge'=>$categoriesAge,'categories'=>$categories,  'ambiances'=>$ambiance);
+        return json_encode($questions);
     }
 }
