129617c7b31c4fdfb6cbeb21ef29a07c92cb324c
Mouhcine Tkhili <tkhilimouhcine@gmail.com>
2019-06-04 10:02:30 -0300

    rename col province

--- app/Http/Controllers/ProvincesPaysController.php
+++ app/Http/Controllers/ProvincesPaysController.php
@@ -11,8 +11,11 @@ class ProvincesPaysController extends Controller
     {
         try
         {
-            $provinces = Province::select ('province') ->where ('pays_id', $id)->get();
-            return json_encode($provinces);
+            $provinces = Province::select ('id','province') ->where ('pays_id', $id)->get();
+
+            $listProv = array('provinces'=>$provinces);
+
+            return json_encode($listProv);
         }
         catch(QueryException $ex){
             dd($ex->getMessage());
